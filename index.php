9434e12c6f57e2fbc9028b5ebfef9a2817adc9be
Muhammad Sultan Alhakim <msultanalhakim@gmail.com>
2023-12-09 14:58:22 +0700

    Adding index and style.css

--- index.php
+++ index.php
@@ -1,1 +1,7 @@
-
+<html>
+    <head>
+        <title></title>
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="assets/style/style.css">
+    </head>
+</html>
